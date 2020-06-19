a1a0d5137e41295589f1a724e44ab3dc3a4f866c
Lucas Figueiredo <lukas.liberato14@gmail.com>
2020-06-19 20:40:09 -0300

    problem with App.js fixed

--- goreactmodule3/src/TodoList.js
+++ goreactmodule3/src/TodoList.js
@@ -1,8 +1,10 @@
+/* eslint-disable max-len */
 import React from 'react';
 
 import { connect } from 'react-redux';
 
-const TodoList = (props) => <ul> { props.todos.map( todo => <li key={ todo.id } > { todo.text } </li>) } </ul>;
+// eslint-disable-next-line react/jsx-one-expression-per-line
+const TodoList = (props) => <ul> {props.todos.map(todo => <li key={todo.id}> { todo.text } </li>)} </ul>;
 
 const mapStateToProps = (state) => ({
   todos: state.todos,
