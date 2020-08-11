a02e1cda538307a9e7dd5658584e1160d116e65d
Lucas Figueiredo <lukas.liberato14@gmail.com>
2020-08-11 11:32:37 -0300

    08 - Nova aplicação

--- goreactmodule3/src/store/index.js
+++ goreactmodule3/src/store/index.js
@@ -1,9 +1,26 @@
+// import { createStore, compose, applyMiddleware } from 'redux';
+
+// import reducers from './reducers';
+
+// const createAppropriateStore =
+//    process.env.NODE_ENV  === 'development' ? console.tron.createStore : createStore;
+
+// const store = createAppropriateStore(reducers, compose(applyMiddleware(...[])));
+
+// export default store;
+
+
 import { createStore, compose, applyMiddleware } from 'redux';
 
 import reducers from './reducers';
 
-const createAppropriateStore = process.env.NODE_ENV  === 'development' ? console.tron.createStore : createStore;
+const composer = process.env.NODE_ENV === 'development'
+  ? compose(
+    applyMiddleware(...[]),
+    console.tron.createEnhancer(),
+  )
+  : applyMiddleware(...[]);
 
-const store = createAppropriateStore(reducers, compose(applyMiddleware(...[])));
+const store = createStore(reducers, composer);
 
 export default store;
